c6215e4a2b91f0d3140b58569d5ec9d715c1f633
Laxmikant B sangolagi <laxmikantsangolagi948@gmail.com>
2026-02-27 20:20:50 +0530

    Update graps.py

--- graps.py
+++ graps.py
@@ -1,4 +1,5 @@
-#This program allows the user to move in four directions (right, up, down, left) and plots the path taken on a graph. The user can input their choice of direction, and the program will update the coordinates accordingly and display the graph after each move.
+#This program allows the user to move in four directions (right, up, down, left) and plots the path taken on a graph. 
+#The user can input their choice of direction, and the program will update the coordinates accordingly and display the graph after each move.
 
 import matplotlib.pyplot as plt
 
@@ -40,3 +41,41 @@ for i in range(10):
             a = a - 1
             Inc(a, b)
             grap(X, Y)
+
+
+
+# This program calculates the Body Mass Index (BMI) based on the user's height and weight, and also plots a pie chart showing the distribution of different BMI categories. 
+# The user is prompted to enter their height in centimeters, weight in kilograms, and the amount of water they drank in liters. The BMI is calculated using the formula: 
+# BMI = weight (kg) / (height (m))^2. The program then displays the calculated BMI and shows a pie chart with the distribution of BMI categories.
+
+import matplotlib.pyplot as plt
+import numpy as np
+content=[
+"Severe Thinness",
+"Moderate Thinness",
+"Mild Thinness",
+"Normal",
+"Overweight",
+"Obese Class I",
+"Obese Class II",
+"Obese Class III"]
+# print(content)
+weight=[5,10,15,31,15,10,9,5]
+
+plt.pie(weight,labels=content)
+plt.show()
+
+a=int(input("enter the hight in cm :"))
+b=int(input("Enter the weight in kg: "))
+water=int(input("how much water you drinked today in liter: "))
+def BMICal(h,w):
+    w=pow(w,2)
+    bmi1=h/w
+    bmi=bmi1*1000
+    print(bmi)
+    return bmi
+
+def user(bmi=0):
+    print("hello !")
+    BMICal(a,b)
+    
