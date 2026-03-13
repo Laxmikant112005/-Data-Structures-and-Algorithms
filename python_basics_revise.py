bc62989a555e0be40654223996cc0baa32a591f0
Laxmikant B sangolagi <laxmikantsangolagi948@gmail.com>
2026-03-13 22:43:47 +0530

    Update python_basics_revise.pyAdd Python program to print palindromic number triangle pattern
    This commit adds a Python program that prints a palindromic number triangle pattern.
    
    The program uses nested loops to generate the pattern:
    
    The first loop prints numbers in increasing order from 1 to the current row number.
    
    The second loop prints numbers in decreasing order to create the palindrome effect.
    
    Each row forms a symmetric pattern of numbers.
    
    Example output for n = 5:
    
    1
    121
    12321
    1234321
    123454321
    
    This implementation demonstrates the use of nested loops and pattern printing in Python.

--- python_basics_revise.py
+++ python_basics_revise.py
@@ -9,3 +9,23 @@ class Dog:
 d1 = Dog("Buddy", 3)
 # Call the bark method
 d1.bark()
+
+
+#......................
+# patterns problem 
+#......................
+
+n = 5
+
+for i in range(1, n + 1):
+    
+    # increasing numbers
+    for j in range(1, i + 1):
+        print(j, end="")
+    
+    # decreasing numbers
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+    
+    print()
+
