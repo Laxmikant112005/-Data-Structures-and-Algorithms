1dd60cbfcb10eff0ff2815f83f7e07a2fabaf56a
Laxmikant B sangolagi <laxmikantsangolagi948@gmail.com>
2026-03-15 23:24:13 +0530

    Add left-aligned star pattern using nested loops
    Implemented a simple Python program to print a left-aligned triangle star pattern using nested loops. 
    Each row prints an increasing number of stars based on the loop index.

--- python_basics_revise.py
+++ python_basics_revise.py
@@ -150,3 +150,15 @@ for i in range(1, n + 1):
 
 # 3.  ......................................
 
+n = 4
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+
+# OUT PUT 
+
+# * 
+# * * 
+# * * * 
+# * * * * 
