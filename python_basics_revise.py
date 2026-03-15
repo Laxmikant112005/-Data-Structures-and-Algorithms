4133c70a4b209228516835e99aca5ef5b0cff15b
Laxmikant B sangolagi <laxmikantsangolagi948@gmail.com>
2026-03-15 23:21:59 +0530

    Update python_basics_revise.pyAdd right-aligned star triangle pattern
    Implemented a Python program to print a right-aligned triangle star pattern using nested loops.
    The first loop prints leading spaces and the second loop prints stars for each row.
    Example output for n = 4:
    
          *
        * *
      * * *
    * * * *

--- python_basics_revise.py
+++ python_basics_revise.py
@@ -128,3 +128,25 @@ for i in range(1, n + 1):
     
     print()
 
+# 2. .................................
+
+n = 4
+
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print("  ", end="")
+    for k in range(i):
+        print("* ", end="")
+
+    print()
+
+# OUT PUT :
+
+#       * 
+#     * * 
+#   * * * 
+# * * * * 
+
+
+# 3.  ......................................
+
